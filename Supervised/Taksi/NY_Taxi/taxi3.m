9d668d3ca36e847e33cdc460282ba0722cb89dcc
MertKara2000 <64528694+MertKara2000@users.noreply.github.com>
2021-07-06 16:23:43 +0300

    Delete Supervised directory

--- Supervised/Taksi/NY_Taxi/taxi3.m
+++
@@ -1,9 +0,0 @@
-load('loc.mat')
-
-for i=1:stime
-    
-    C = split(Time(i),' ')
-    C(3) = []
-    Time(i) = strcat(C(1 ),C(2))
-end
-
--- Supervised/Taksi/NY_Taxi/taxi4.m
+++
@@ -1,8 +0,0 @@
-load('time2.mat')
-
-for i=1:stime
-    
-    C = split(Time(i),':')
-    C(3) = []
-    Time(i) = C(1)
-end
