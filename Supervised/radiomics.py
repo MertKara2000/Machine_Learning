94ec9111efc1acd7fda35c82f06ecbef60ffd84d
MertKara2000 <64528694+MertKara2000@users.noreply.github.com>
2021-07-06 16:20:20 +0300

    Update radiomics.py

--- Supervised/radiomics.py
+++ Supervised/radiomics.py
@@ -58,7 +58,7 @@ X_train, X_test, y_train, y_test = train_test_split( X, y, test_size=0.2, random
 
 classificationType = "Control/Pf,NonPf"
 cv = 10
-max_feature_num = 31
+max_feature_num = 10
 
 classificationList = list()
 featureSelectionList = list()
